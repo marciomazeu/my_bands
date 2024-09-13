33d1c3c6040fdcb8b661222f882b57cf2bd44a2b
marciomazeu <mazeu911@gmail.com>
2024-09-12 21:39:09 -0400

    Project started. Created a title and some menus

---
+++ my_bands/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_bands
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            showMenu();
+
+
+            void showMenu()
+            {
+                Console.WriteLine("**********************");
+                Console.WriteLine("MY BANDS");
+                Console.WriteLine("**********************");
+
+                Console.WriteLine("Press 1 to insert a new BAND.");
+                Console.WriteLine("Press 2 to evaluate a BAND.");
+                Console.WriteLine("Press 3 to see the average score of a BAND.");
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
