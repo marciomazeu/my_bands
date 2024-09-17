e429693cebf2aae2a6a6b38ac3637e64faeb87eb
marciomazeu <mazeu911@gmail.com>
2024-09-16 22:15:01 -0400

    Created a function to insert a new band. Added a new menu item to show the bands saves.

--- my_bands/Program.cs
+++ my_bands/Program.cs
@@ -12,7 +12,7 @@ namespace my_bands
         {
             showMenu();
 
-
+           
             void showMenu()
             {
                 Console.WriteLine("**********************");
@@ -20,12 +20,39 @@ namespace my_bands
                 Console.WriteLine("**********************");
 
                 Console.WriteLine("Press 1 to insert a new BAND.");
-                Console.WriteLine("Press 2 to evaluate a BAND.");
-                Console.WriteLine("Press 3 to see the average score of a BAND.");
+                Console.WriteLine("Press 2 to show the BAND list.");
+                Console.WriteLine("Press 3 to evaluate a BAND.");
+                Console.WriteLine("Press 4 to see the average score of a BAND.");
+
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        //Console.WriteLine("Voce escolheu a opcao 1.");
+                        insertBand();
+                        break;
+                    case 2:
+                        Console.WriteLine("Voce sescolheu a opcao 2.");
+                        break;
+                    default:
+                        Console.WriteLine("Opcao desconhecida");
+                        break;
+                }
 
                 Console.ReadKey();
             }
 
+           void insertBand()
+            {
+                Console.WriteLine("Write the name of your band to insert.");
+                string bandName = Console.ReadLine();
+                List<string> band = new List<string>();
+                band.Add(bandName);
+                Console.WriteLine($"Voce inseriu a banda {bandName} com sucesso.");
+
+                Console.WriteLine("\nPress any key to exit.");
+            }
         }
     }
 }
